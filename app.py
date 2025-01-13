e6675b019574b963137dad3404337881828bb77d
Lucas Galhardo <98436217+LucasKiller@users.noreply.github.com>
2025-01-13 04:17:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ def home():
 
 @app.route('/callback')
 def callback():
-    """Rota para processar o código retornado pelo Instagram"""
     code = request.args.get('code')
     if not code:
         return jsonify({"error": "Nenhum código retornado pelo Instagram."}), 400
@@ -47,7 +46,7 @@ def callback():
     user_id = response.json().get("user_id")
 
     # Obter informações do perfil
-    profile_url = f"https://graph.instagram.com/me?fields=id,username,account_type,media_count,followers_count&access_token={access_token}"
+    profile_url = f"https://graph.instagram.com/me?fields=id,username,account_type,media_count&access_token={access_token}"
     profile_response = requests.get(profile_url)
 
     if profile_response.status_code != 200:
